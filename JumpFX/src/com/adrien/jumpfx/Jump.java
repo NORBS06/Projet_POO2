09c075b32a8199d3194eda2df41dc5079d988216
norbs06 <adriennorbert32@gmail.com>
2024-02-07 11:57:08 +0400

    game files setup

---
+++ JumpFX/src/com/adrien/jumpfx/Jump.java
@@ -0,0 +1,24 @@
+package com.adrien.jumpfx;
+
+import java.util.Random;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Jump extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
